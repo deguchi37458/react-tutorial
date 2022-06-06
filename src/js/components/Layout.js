a627693f40df5c16d5f08530d0d98d268f8c3ed9
deguchi37458 <deguchi37458@gmail.com>
2022-06-06 14:13:20 +0900

    init react router

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,21 +1,10 @@
 import React from "react";
-import Header from "./Header"
-import Footer from "./Footer"
+
 
 export default class Layout extends React.Component {
-	constructor() {
-		super();
-		this.state = {title: "Welcome"}
-	}
-	changeTitle(title) {
-		this.setState({title});
-	}
-	render() {
-		return (
-			<div>
-				<Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
-				<Footer />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <h1>KillerNews.net</h1>
+    );
+  }
 }
