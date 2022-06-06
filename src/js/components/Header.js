0fb63dbfb22fa84d35833fd78d580b3215f9ce72
deguchi37458 <deguchi37458@gmail.com>
2022-06-06 12:59:24 +0900

    update react tutorial

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -2,10 +2,15 @@ import React from "react";
 import Title from "./Header/Title"
 
 export default class Header extends React.Component {
-render() {
+  handleChange(e) {
+    const title = e.target.value;
+    this.props.changeTitle(title);
+  }
+  render() {
     return (
       <div>
-      <Title />
+        <Title title={this.props.title}/>
+        <input value={this.props.title} onChange={this.handleChange.bind(this)} />
       </div>
     );
   }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -3,11 +3,17 @@ import Header from "./Header"
 import Footer from "./Footer"
 
 export default class Layout extends React.Component {
+	constructor() {
+		super();
+		this.state = {title: "Welcome"}
+	}
+	changeTitle(title) {
+		this.setState({title});
+	}
 	render() {
 		return (
 			<div>
-				<Header/>
-				<Header/>
+				<Header changeTitle={this.changeTitle.bind(this)} title={this.state.title}/>
 				<Footer />
 			</div>
 		);
